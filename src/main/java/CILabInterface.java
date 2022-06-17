f0075bdeddda841237f3ac3bcb89c716ecf022f9
sarafarag <sara.farag@bellevuecollege.edu>
2022-06-17 12:10:19 -0700

    removed package

--- src/main/java/CILabInterface.java
+++ src/main/java/CILabInterface.java
@@ -1,5 +1,3 @@
-package pack1;
-
 public interface CILabInterface {
 
     /**
---
+++ src/test/java/LabTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import pack1.*;
+
+public class LabTest {
+
+    private CILabInterface myString;
+    @BeforeEach
+    public void setup() {
+        myString = new CILab();
+    }
+
+    @AfterEach
+    public void teardown() {
+        myString = null;
+    }
+
+    @Test
+    public void testMixedCaps() {
+        myString.setString("This String Has Captials");
+        assertTrue(myString.detectCapitalUse());
+    }
+
+    @Test
+    public void testNoCaps() {
+        myString.setString("this string has no captials");
+        assertFalse(myString.detectCapitalUse());
+    }
+
+    @Test
+    public void testAllCaps() {
+        myString.setString("THIS STRING IS ONLY CAPITALS");
+        assertTrue(myString.detectCapitalUse());
+    }
+
+}
